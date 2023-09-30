b673e54e5e3f0b355797914ee9645b2986f2c066
Nunzio Lanza <nunziolanza@Nunzios-MacBook-Air.local>
2023-09-30 01:51:13 -0400

    Added title bar and logo at top. Moved some buttons around. Added logo to top left. git status

--- src/components/Chat/ChatInterface.js
+++ src/components/Chat/ChatInterface.js
@@ -6,61 +6,49 @@ function ChatInterface({ roomImage, children }) {
   const [inputValue, setInputValue] = useState('');
 
   const handleSendMessage = async () => {
-    if (!inputValue.trim()) {
-      return; // Exit if the input is only whitespace or empty.
-    }
-
-    const userMessage = { sender: 'user', content: inputValue };
-    setMessages(prevMessages => [...prevMessages, userMessage]);
-
-    try {
-        const response = await fetch("/chat", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ message: inputValue })
-        });
-
-        // This is where you log the raw server response for debugging
-        const rawResponse = await response.text();
-        console.log("Server Response:", rawResponse);
-
-        // After logging, you try to parse it as JSON (make sure the response is actually valid JSON!)
-        const data = JSON.parse(rawResponse);
-
-        const gpt4Response = { sender: 'gpt-4', content: data.message };
-        setMessages(prevMessages => [...prevMessages, gpt4Response]);
-
-    } catch (error) {
-        console.error("Error calling server:", error);
-    }
-
-    setInputValue('');
-};
-
+    // ... [rest of the handleSendMessage function]
+  };
 
   return (
-    <div style={{ maxWidth: '600px', margin: 'auto', fontFamily: 'Arial, sans-serif' }}>
-      {roomImage && <img src={roomImage} alt="Room View" style={{ width: '100%', marginBottom: '20px' }} />}
-      {children}
-      <div className="chat-window" style={{ border: '1px solid #ccc', padding: '20px', maxHeight: '400px', overflowY: 'scroll', borderRadius: '5px', backgroundColor: '#f4f4f4' }}>
-        {messages.map((msg, index) => (
-          <ChatMessage key={index} message={msg} />
-        ))}
+    <>
+      {/* Black title bar */}
+      <div style={{ backgroundColor: 'black', height: '60px', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white', fontSize: '24px' }}>
+        Title
       </div>
-      <div className="chat-input" style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <input
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Ask me anything..."
-          style={{ width: '80%', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }}
-        />
-        <button onClick={handleSendMessage} style={{ width: '18%', padding: '10px', borderRadius: '5px', backgroundColor: '#007BFF', color: 'white', border: 'none', cursor: 'pointer' }}>
-          Send
-        </button>
+
+      <div style={{ maxWidth: '600px', margin: 'auto', fontFamily: 'Roboto, sans-serif', display: 'flex', flexDirection: 'column', height: 'calc(100vh - 60px)' }}>
+        <img src="/decorAItor_logo-removebg-preview.png" alt="Logo" style={{ position: 'absolute', top: '10px', left: '10px', height: '50px', zIndex: 1 }} />
+        {roomImage && <img src={roomImage} alt="Room View" style={{ width: '100%', marginBottom: '20px', fontFamily: 'Roboto, sans-serif' }} />}
+        {children}
+        <div className="chat-window" style={{ flex: 1, border: '1px solid #ccc', padding: '20px', maxHeight: '400px', overflowY: 'scroll', borderRadius: '5px', backgroundColor: '#f4f4f4', fontFamily: 'Roboto, sans-serif' }}>
+          {messages.map((msg, index) => (
+            <ChatMessage key={index} message={msg} />
+          ))}
+        </div>
+        <div className="chat-input" style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontFamily: 'Roboto, sans-serif' }}>
+          <input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Give me design ideas"
+            style={{ width: '80%', padding: '10px', borderRadius: '5px', border: '1px solid #ccc', fontFamily: 'Roboto, sans-serif' }}
+          />
+          <button 
+            className="robotoFont"
+            onClick={handleSendMessage} 
+            style={{ 
+              width: '18%', 
+              padding: '10px', 
+              borderRadius: '5px', 
+              backgroundColor: 'black', 
+              color: 'white', 
+              border: 'none', 
+              cursor: 'pointer'
+            }}>
+            Design
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
