50325d00204ceb563245df399c7a3dd8a9875724
Ernesto Louis <elouis12@me.com>
2023-09-30 20:28:28 -0400

    .

--- server/model/furnituredata.js
+++ server/model/furnituredata.js
@@ -1,4 +1,4 @@
-let furnituredata = {
+/*let furnituredata = {
 
     "chairs": [
         {
@@ -11,6 +11,26 @@ let furnituredata = {
     "tables": [
 
     ]
+
+}*/
+
+let furnituredata = {
+
+    "chairs": [
+        {
+            title: "Thomas White Side Chair",
+            imageURL: "https://embed.widencdn.net/img/cityfurniture/yv9vgzdtue/1920x1080px/Z2306560095F00_CS_THOMAS_WHT_S_CHR.webp?keep=c&crop=no&quality=80&u=m2xgp2%201x,%20https://embed.widencdn.net/img/cityfurniture/yv9vgzdtue/3840x2160px/Z2306560095F00_CS_THOMAS_WHT_S_CHR.webp?keep=c&crop=no&quality=80&u=m2xgp2%202x",
+            productURL: "https://www.cityfurniture.com/product/6560095/thomas-white-side-chair",
+            description: "The Thomas side chair adds class to any dining room. Its chic design features leatherette upholstery with horizontal stitching and chrome toothpick legs, elevating your home with ease. Let your guests sit comfortably with the foam seat and back cushion. Thomas is perfect for Miami modern dining spaces.",
+            price: 79.95
+        },
+    ],
+
+    "tables": [
+
+    ]
+
+
 
 }
 
